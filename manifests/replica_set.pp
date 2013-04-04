4742246207ff85616dad805ab5801b1d85b0df8f
Konstantin Krauss <konstantin@propertybase.com>
2013-04-04 10:28:34 +0200

    Add shell script for repica set member management

--- manifests/replica_set.pp
+++ manifests/replica_set.pp
@@ -4,18 +4,44 @@ define mongodb::replica_set(
 ) {
   validate_hash($repl_config)
 
-  if $repl_config['_id'] == 0 {
+  $my_repl_config = $repl_config['nodes'][$::hostname]
+  $first_node_name = keys($repl_config['nodes'])[0]
+
+  if $my_repl_config['_id'] == 0 {
     exec { "initiate replica set":
       command => 'mongo --eval "rs.initiate()"',
       onlyif  => 'test `mongo --eval "rs.conf()" | grep "null"`',
-      before  => Notify['replica set initiated'],
-    }
-
-    notify {'replica set initiated':
-      message => "Replica Set is initiated with rs.initiate() on ${$::hostname}"
     }
   }
   else {
-    notify{"Replica Set will not be initiated on this host.":}
+    #exec { "add member to set":
+    #  command => "/root/bin/mongo_helper/add_members.sh ${first_node_name} ${my_repl_config['_id']} ${::hostname}.${::hostname_base} ${my_repl_config['priority']}",
+    #  onlyif  => ""
+    #  require => File['/root/bin/mongo_helper/add_members.sh'],
+    #}
+  }
+
+  file { "/root/bin":
+    ensure => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+  }
+
+  file { '/root/bin/mongo_helper':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/root/bin']
+  }
+
+  file { '/root/bin/mongo_helper/add_members.sh':
+    ensure  => present,
+    content => template("${module_name}/add_members.sh.erb"),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File['/root/bin/mongo_helper']
   }
 }
